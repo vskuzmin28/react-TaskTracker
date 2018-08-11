f5eb5b638f0bbc3a4f32aab9090d348e35cf50bc
vskuzmin28 <vskuzmin28@gmail.com>
2018-08-11 22:19:32 +0300

    add authorization

--- src/App.js
+++ src/App.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import Auth from './components/Auth'
 import Register from './components/Register'
 import Index from './components/Index'
 
-import WithLoggedIn from './HoC/WithLoggedIn'
+import RouteForLoggedIn from './HoC/RouteForLoggedIn'
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
+      users: [{
+        name: 'test',
+        pass: '123',
+      }],
+      loggedUserName: '',
       isLoggedIn: false,
     }
+
+    this.enteredName = ''
+    this.enteredPass = ''
   }
 
+  onNameEnter = (e) => {
+    this.enteredName = e.target.value
+  }
+
+  onPassEnter = (e) => {
+    this.enteredPass = e.target.value
+  }
+
+  checkCredentials = (e) => {
+    e.preventDefault()
+
+    const isCredentialsCorrect = this.state.users.some(({name, pass}) => 
+      (name === this.enteredName) && (pass === this.enteredPass)
+    )
+
+    if (isCredentialsCorrect) {
+      this.setState({
+        loggedUserName: this.enteredName,
+        isLoggedIn: true,
+      })
+    }
+  }
 
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <WithLoggedIn 
+          <RouteForLoggedIn 
             exact 
             path="/"
             isLoggedIn={this.state.isLoggedIn}
             component={Index}
           />
-          <Route path="/auth" component={Auth} />
+          <Route 
+            path="/auth"
+            render={renderProps => {
+              const props = Object.assign({}, renderProps, {
+                onNameEnter: this.onNameEnter,
+                onPassEnter: this.onPassEnter,
+                checkCredentials: this.checkCredentials,
+              })
+
+              return this.state.isLoggedIn
+                ? <Redirect
+                    to={{ pathname: '/', state: { from: props.location } }}
+                  />
+                : <Auth {...props} />
+            }}
+          />
           <Route path="/register" component={Register} />
         </Switch>
       </BrowserRouter>
--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
 const Auth = (props) => {
+  console.log(props)
   return (
     <div className="block-authorization">
       <div className="container">
         <div className="auth-form">
           <h2>Добро пожаловать в UI TASK TRACKER</h2>
           <h3>Введите логин/пароль для просмотра своих задач</h3>
-          <form action="/" method="post">
-            <input name="" type="text" placeholder="Логин"/>
-            <input name="" type="password" placeholder="Пароль"/>
-            <button className="btn-orange">Войти</button>
+          <form method="POST" onSubmit={props.checkCredentials}>
+            <input 
+              id="login" 
+              name="login" 
+              type="text" 
+              placeholder="Логин"
+              onChange={props.onNameEnter}
+            />
+            <input 
+              id="pass" 
+              name="pass" 
+              type="password" 
+              placeholder="Пароль"
+              onChange={props.onPassEnter}
+            />
+            <button className="btn-orange" type="submit">Войти</button>
           </form><a href="registration.html" title="">Зарегистрироваться</a><img src="../img/logo-white.png" alt="@@"/>
         </div>
       </div>
